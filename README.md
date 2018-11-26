daf45d53456f814f4b5b55c2884e2d2600d6888b
Sarun Rattanasiri <midnight_w@gmx.tw>
2018-11-27 00:50:15 +0700

    add the button debounce library code

---
+++ README.md
@@ -0,0 +1,53 @@
+# Button Debounce
+
+## What is it?
+
+Button Debounce is a library to handle [contact bounce](https://en.wikipedia.org/wiki/Switch#Contact_bounce) in your microcontroller system circuit. It build with event driven architecture, non-blocking execution, only standard C, platform and hardware independent. And I hope you'd enjoy it.
+
+## Example usage
+
+This is an example usage with an STM8 microcontroller.  
+The library itself is platform and hardware independent you can use it with Arduino, MCS-51, ARM Cortex-M, you name it.
+The example uses the library to detect button press signal and toggle the LED on the hardware it runs on.
+```c
+#define STM8S103
+#include "stm8s.h"
+
+// it is a unconventional to include c directly without linking
+// but this method suite the embedded system more IMHO
+// this library design such that it is unlikely for name clashing
+#include "button_debounce.c"
+
+// configure callbacks
+const ButtonDebounce_Config button_c3_debounce_config = {
+    .falled = &led_toggle, // here we subscribe to "falled" event
+    // apart from "falled" there are "rised" and "state_changed"
+};
+static ButtonDebounce_State button_c3_debounce_state;
+
+int main() {
+  // configure pins
+  GPIOB->DDR = 0x20;
+  GPIOC->CR1 = 0x08;
+  GPIOB->ODR |= 0x20;
+
+  button_debounce__state_init(&button_c3_debounce_state);
+
+  for (;;) {
+    // keep sample (poll) the state of the button and feed it to
+    // the library
+    button_debounce__sample(&button_c3_debounce_config,
+                            &button_c3_debounce_state, GPIOC->IDR & 0x08);
+  }
+}
+
+// subscribe to the event
+void led_toggle() {
+  // we just toggle an LED on pin B5 here
+  GPIOB->ODR ^= 0x20;
+}
+```
+
+## License
+
+Button Debounce is released under the [MIT License](https://opensource.org/licenses/MIT).
---
+++ inc/button_debounce.c
@@ -0,0 +1,124 @@
+#ifndef uint8_t
+#define uint8_t unsigned char
+#endif
+
+#ifndef BUTTON_DEBOUNCE__CONFIRM
+#define BUTTON_DEBOUNCE__CONFIRM 100
+#endif
+
+#ifndef __BUTTON_DEBOUNCE__
+#define __BUTTON_DEBOUNCE__
+
+typedef enum _button_debounce__button_state {
+  BUTTON_DEBOUNCE__LOW,
+  BUTTON_DEBOUNCE__HIGH,
+} ButtonDebounce_ButtonStatus;
+
+typedef struct _button_debounce__config {
+  void (*state_changed)(ButtonDebounce_ButtonStatus state);
+  void (*rised)();
+  void (*falled)();
+} ButtonDebounce_Config;
+
+typedef enum _button_debounce__internal_status {
+  _BUTTON_DEBOUNCE__UNKNOWN,
+  _BUTTON_DEBOUNCE__UNKNOWN_TO_HIGH,
+  _BUTTON_DEBOUNCE__UNKNOWN_TO_LOW,
+  _BUTTON_DEBOUNCE__HIGH,
+  _BUTTON_DEBOUNCE__LOW,
+  _BUTTON_DEBOUNCE__HIGH_TO_LOW,
+  _BUTTON_DEBOUNCE__LOW_TO_HIGH,
+} _ButtonDebounce_InternalStatus;
+
+typedef struct _button_debounce__state {
+  _ButtonDebounce_InternalStatus status;
+  uint8_t confirmation_count;
+} ButtonDebounce_State;
+
+#define button_debounce__state_init(state)       \
+  do {                                           \
+    (state)->status = _BUTTON_DEBOUNCE__UNKNOWN; \
+    (state)->confirmation_count = 0;             \
+  } while (0)
+
+void button_debounce__sample(ButtonDebounce_Config* config,
+                             ButtonDebounce_State* state,
+                             uint8_t button_state) {
+  switch (state->status) {
+    case _BUTTON_DEBOUNCE__HIGH:
+      if (!button_state) {
+        state->status = _BUTTON_DEBOUNCE__HIGH_TO_LOW;
+        state->confirmation_count = 0;
+      }
+      break;
+    case _BUTTON_DEBOUNCE__LOW:
+      if (button_state) {
+        state->status = _BUTTON_DEBOUNCE__LOW_TO_HIGH;
+        state->confirmation_count = 0;
+      }
+      break;
+    case _BUTTON_DEBOUNCE__HIGH_TO_LOW:
+      if (button_state) {
+        state->status = _BUTTON_DEBOUNCE__HIGH;
+      } else {
+        if (state->confirmation_count++ > BUTTON_DEBOUNCE__CONFIRM) {
+          state->status = _BUTTON_DEBOUNCE__LOW;
+          if (config->falled) {
+            config->falled();
+          }
+          if (config->state_changed) {
+            config->state_changed(BUTTON_DEBOUNCE__LOW);
+          }
+        }
+      }
+      break;
+    case _BUTTON_DEBOUNCE__LOW_TO_HIGH:
+      if (button_state) {
+        if (state->confirmation_count++ > BUTTON_DEBOUNCE__CONFIRM) {
+          state->status = _BUTTON_DEBOUNCE__HIGH;
+          if (config->rised) {
+            config->rised();
+          }
+          if (config->state_changed) {
+            config->state_changed(BUTTON_DEBOUNCE__HIGH);
+          }
+        }
+      } else {
+        state->status = _BUTTON_DEBOUNCE__LOW;
+      }
+      break;
+    case _BUTTON_DEBOUNCE__UNKNOWN:
+      if (button_state) {
+        state->status = _BUTTON_DEBOUNCE__UNKNOWN_TO_HIGH;
+      } else {
+        state->status = _BUTTON_DEBOUNCE__UNKNOWN_TO_LOW;
+      }
+      state->confirmation_count = 0;
+      break;
+    case _BUTTON_DEBOUNCE__UNKNOWN_TO_HIGH:
+      if (button_state) {
+        if (state->confirmation_count++ > BUTTON_DEBOUNCE__CONFIRM) {
+          state->status = _BUTTON_DEBOUNCE__HIGH;
+          if (config->state_changed) {
+            config->state_changed(BUTTON_DEBOUNCE__HIGH);
+          }
+        }
+      } else {
+        state->status = _BUTTON_DEBOUNCE__UNKNOWN;
+      }
+      break;
+    case _BUTTON_DEBOUNCE__UNKNOWN_TO_LOW:
+      if (button_state) {
+        state->status = _BUTTON_DEBOUNCE__UNKNOWN;
+      } else {
+        if (state->confirmation_count++ > BUTTON_DEBOUNCE__CONFIRM) {
+          state->status = _BUTTON_DEBOUNCE__LOW;
+          if (config->state_changed) {
+            config->state_changed(BUTTON_DEBOUNCE__LOW);
+          }
+        }
+      }
+  }
+}
+
+#endif
